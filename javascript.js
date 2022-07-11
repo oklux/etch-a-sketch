3bcd29ad2c220df557d0ebeed975814f64de1411
oklux <kaleeppik@gmail.com>
2022-07-11 14:31:53 +0300

    Added rainbow button

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
 const grid = document.querySelector('.grid');
+const slider = document.getElementById("slider");
+const gridLabel = document.getElementById('grid-label');
+const rainbow = document.querySelector('#rainbow');
 
 let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true)
@@ -6,6 +9,7 @@ document.body.onmouseup = () => (mouseDown = false)
 
 
 function createGrid(size) {
+    removeCells();
     for (let i = 0; i < size;i++) {
             const row = document.createElement('div');
             row.classList.add('row')
@@ -22,16 +26,44 @@ function createGrid(size) {
         
 }
 
+function removeCells() {
+    while(grid.firstChild) {
+      grid.removeChild(grid.firstChild);
+    }
+  }
+
 let currentMode = "color";
 let currentColor = "black";
 
 function changeColor(e) {
     if (e.type === 'mouseover' && !mouseDown) return
     e.preventDefault();
-    if (currentMode === 'color') {
+    if (currentMode === 'rainbow') {
+        e.target.style.backgroundColor = getRandomRgb();
+    } else if (currentMode === 'color') {
       e.target.style.backgroundColor = currentColor;
     }
 }
+function getRandomRgb() {
+    var num = Math.round(0xffffff * Math.random());
+    var r = num >> 16;
+    var g = num >> 8 & 255;
+    var b = num & 255;
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+  }
+rainbow.addEventListener('click', function() {
+    if (currentMode === 'rainbow') {
+        rainbow.classList.remove('clickbutton');
+        currentMode = 'color';
+        return;
+    }
+    rainbow.classList.add('clickbutton');
+    currentMode = 'rainbow';
+});
 
-createGrid(16);
+slider.addEventListener("change", function() { 
+    createGrid(slider.value);
+    gridLabel.textContent = slider.value + " x " + slider.value;
+});
 
+createGrid(slider.value);
